62ceadf882cc7bda15ad5da356b288275f4a7e7c
unknown <veronicamfalconi@gmail.com>
2021-02-01 16:42:13 -0300

    Segundo commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Bienvenidos DivA");
+	alert("Bienvenidos División A");
 }
 
